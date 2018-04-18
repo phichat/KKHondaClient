92ba97df453bffc2474fd1a7921791f68e5e75f5
Phichat K <phichat_dk@hotmail.com>
2018-04-18 14:38:04 +0700

    Initial commit

---
+++ src/app/directives/icheck.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, ElementRef } from '@angular/core';
+declare var jQuery: any;
+
+@Directive({
+  selector: '[appIcheck]'
+})
+export class IcheckDirective {
+
+  jQuery: any = jQuery;
+
+  constructor(el: ElementRef) {
+    this.jQuery(el.nativeElement).iCheck({
+      checkboxClass: 'icheckbox_square-green',
+      radioClass: 'iradio_square-green'
+    });
+  }
+
+}
---
+++ src/app/views/appviews/appviews.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+
+// Modules
+import { IboxtoolsModule } from '../../components/common/iboxtools/iboxtools.module';
+
+// Components
+import { StarterViewComponent } from "./starterview.component";
+import { LoginComponent } from "./login.component";
+
+import { PeityModule } from '../../components/charts/peity';
+import { SparklineModule } from '../../components/charts/sparkline';
+import { HomeComponent } from './home/home.component';
+import { StarterComponent } from './starter/starter.component';
+import { SalesComponent } from './sales/sales.component';
+import { BookingComponent } from './booking/booking.component';
+
+// Directives
+import { IcheckDirective } from "../../directives/icheck.directive";
+import { FootableDirective } from "../../directives/footable.directive";
+
+@NgModule({
+  declarations: [
+    // Components
+    StarterViewComponent,
+    LoginComponent,
+    HomeComponent,
+    StarterComponent,
+    SalesComponent,
+    BookingComponent,
+
+    // Directives
+    IcheckDirective,
+    FootableDirective
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule,
+    PeityModule,
+    SparklineModule,
+    IboxtoolsModule
+  ],
+  exports: [
+    StarterViewComponent,
+    LoginComponent
+  ],
+})
+
+export class AppviewsModule {
+}
