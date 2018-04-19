a8abd342db6a8a602663a5d9d0fb026a8335d311
Phichat K <phichat_dk@hotmail.com>
2018-04-19 22:55:54 +0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {RouterModule} from '@angular/router';
-import {LocationStrategy, HashLocationStrategy} from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
-import {ROUTES} from "./app.routes";
+import { ROUTES } from "./app.routes";
 import { AppComponent } from './app.component';
 
 // App views
-import {DashboardsModule} from "./views/dashboards/dashboards.module";
-import {AppviewsModule} from "./views/appviews/appviews.module";
+import { DashboardsModule } from "./views/dashboards/dashboards.module";
+import { AppviewsModule } from "./views/appviews/appviews.module";
 
 // App modules/components
-import {LayoutsModule} from "./components/common/layouts/layouts.module";
+import { LayoutsModule } from "./components/common/layouts/layouts.module";
 import { ModalCustomerComponent } from './components/common/modal-customer/modal-customer.component';
 import { SelectizeDirective } from './directives/selectize.directive';
 
@@ -32,7 +32,7 @@ import { SelectizeDirective } from './directives/selectize.directive';
     AppviewsModule,
     RouterModule.forRoot(ROUTES),
   ],
-  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
