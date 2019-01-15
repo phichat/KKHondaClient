98806cebbe7509842a184422ecf176d5a36fe9d5
Phichat K <phichat_dk@hotmail.com>
2019-01-15 21:07:28 +0700

    commit

--- src/app/components/common/page-load-warpper/page-load-warpper.component.ts
+++ src/app/components/common/page-load-warpper/page-load-warpper.component.ts
@@ -1,28 +1,28 @@
-import { Component, OnInit, OnDestroy, AfterViewInit, AfterViewChecked, AfterContentChecked } from '@angular/core';
-declare var jQuery: any;
+// import { Component, OnInit, OnDestroy, AfterViewInit, AfterViewChecked, AfterContentChecked } from '@angular/core';
+// declare var jQuery: any;
 
-@Component({
-  selector: 'app-page-load-warpper',
-  templateUrl: './page-load-warpper.component.html',
-  styleUrls: ['./page-load-warpper.component.scss']
-})
-export class PageLoadWarpperComponent implements OnInit, AfterViewInit {
+// @Component({
+//   selector: 'app-page-load-warpper',
+//   templateUrl: './page-load-warpper.component.html',
+//   styleUrls: ['./page-load-warpper.component.scss']
+// })
+// export class PageLoadWarpperComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+//   constructor() { }
 
-  ngOnInit() {
-  }
+//   ngOnInit() {
+//   }
 
-  ngAfterViewInit() {
-    // setTimeout(() => {
-      // jQuery('.page-loader-wrapper').addClass('animated fadeOutRight');
+//   ngAfterViewInit() {
+//     // setTimeout(() => {
+//       // jQuery('.page-loader-wrapper').addClass('animated fadeOutRight');
 
-      // setTimeout(() => {
-      //   jQuery('.page-loader-wrapper')
-      //     .css('display', 'none')
-      // }, 500);
+//       // setTimeout(() => {
+//       //   jQuery('.page-loader-wrapper')
+//       //     .css('display', 'none')
+//       // }, 500);
 
-    // }, 500);
-  }
+//     // }, 500);
+//   }
 
-}
+// }
--- src/app/directives/inputmask.directive.ts
+++ src/app/directives/inputmask.directive.ts
@@ -1,24 +1,24 @@
-import { Directive, ElementRef, AfterViewInit, OnInit } from '@angular/core';
-declare var jQuery: any;
+// import { Directive, ElementRef, AfterViewInit, OnInit } from '@angular/core';
+// declare var jQuery: any;
 
-@Directive({
-  selector: '[appInputmask]'
-})
-export class InputmaskDirective implements OnInit {
+// @Directive({
+//   selector: '[appInputmask]'
+// })
+// export class InputmaskDirective implements OnInit {
 
-  constructor(private el: ElementRef) {
+//   constructor(private el: ElementRef) {
   
-  }
+//   }
 
-  ngOnInit() {
-  //   jQuery(this.el.nativeElement).inputmask({
-  //     'alias': 'numeric',
-  //     'groupSeparator': ',',
-  //     'autoGroup': true,
-  //     'digits': 2,
-  //     'digitsOptional': false,
-  //     'placeholder': '0.00'
-  //   });
-  }
+//   ngOnInit() {
+//   //   jQuery(this.el.nativeElement).inputmask({
+//   //     'alias': 'numeric',
+//   //     'groupSeparator': ',',
+//   //     'autoGroup': true,
+//   //     'digits': 2,
+//   //     'digitsOptional': false,
+//   //     'placeholder': '0.00'
+//   //   });
+//   }
 
-}
+// }
