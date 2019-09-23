434b0c79dec2a03fe9789d9b32d1b0eaa38fd6da
Phichat K <phichat_dk@hotmail.com>
2019-09-23 12:06:19 +0700

    commit

--- src/app/views/appviews/ris/tag-al-form/tag-al.config.ts
+++ src/app/views/appviews/ris/tag-al-form/tag-al.config.ts
@@ -4,6 +4,7 @@ import { DropDownModel } from 'app/models/drop-down-model';
 import { AlStatus } from 'app/entities/ris.entities';
 import { RisConfig } from '../ris.config';
 import { IUserResCookie } from 'app/interfaces/users';
+import { Input } from '@angular/core';
 
 export class TagAlConfig extends RisConfig {
     get SedList(): FormArray {
